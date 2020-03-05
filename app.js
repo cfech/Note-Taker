209f13a81a84ffe63293a1c4c4976a96822db6f0
Connor <cfech44@gmail.com>
2020-03-05 10:23:09 -0500

    adding public folder connection and delete aspects

--- app.js
+++ app.js
@@ -1,23 +1,32 @@
 const fs = require('fs')
 var express = require("express");
 var path = require("path");
+// var index = require("/public/assets/js/index.js")
 
-// var dbJson = require("db/db.json")
+var x = 1
+
+
+// console.log(index)
+// // var dbJson = require("db/db.json")
 
 var app = express(); // runs an express server
 var PORT = 3000; // its port
 
 // notes array 
-var notes = [{
-    routeName: "1",
-    name: "Title",
-    content: "note contents",
-    uniqueid: 1253
-}]
+var notes = [
+
+
+]
 
 //Middle wear
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(__dirname + '/public'));
+
+
+
+
+
 
 //Get routes
 app.get("/", function (req, res) {
@@ -28,7 +37,7 @@ app.get("/notes", function (req, res) {
 });
 
 
-//have to do something with db json here?
+
 app.get("/api/notes", function (req, res) {
     res.json(notes);;
 });
@@ -38,8 +47,10 @@ app.get("/api/notes", function (req, res) {
 app.post("/api/notes", function (req, res) {
 
     let newNote = req.body;
+    console.log(newNote)
 
-    newNote.routeName = newNote.name.replace(/\s+/g, "").toLowerCase();
+    newNote.routeName = newNote.title.replace(/\s+/g, "").toLowerCase();
+    newNote.id = x++
 
     console.log(newNote);
 
@@ -51,19 +62,22 @@ app.post("/api/notes", function (req, res) {
 
 
 //delete function 
-// app.delete(`/api/notes/:id`, function(req , res){
+app.delete(`/api/notes/:id`, function (req, res) {
 
-//     var chosen = req.params.character;
-  
-//     console.log(chosen);
-  
-//     for (var i = 0; i < notes.length; i++) {
-//       if (chosen === notes[i].routeName) {
-//         return res.json(notes[i]);
-//       }
-//     }
+    var noteId = req.params.id;
 
-// })
+    for (var i = 0; i < notes.length; i++) {
+        console.log(noteId)
+        console.log(notes[i].id)
+
+        if (noteId == notes[i].id) {
+            console.log(noteId)
+            notes.splice(i, 1);
+            console.log(notes)
+        }
+    }
+    res.json(notes)
+})
 
 
 // app.get("/", function (req, res) {
